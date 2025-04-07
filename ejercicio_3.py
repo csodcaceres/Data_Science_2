0f698554a9352b79e259ca4594feb116f8d83100
csodcaceres <cs.odcaceres@gmail.com>
2025-04-07 19:08:35 -0300

    EJ 3

---
+++ ejercicio_3.py
@@ -0,0 +1,7 @@
+# Crea un programa que lea la siguiente lista de números y elija uno al azar.
+
+import random
+
+lista = [8, 12, 54, 23, 43, 1, 90, 87, 105, 77]
+numero_aleatrio = random.choice(lista)
+print(numero_aleatrio)

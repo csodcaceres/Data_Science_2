e268e3c9c34915f96fe62f485b8249eebad966e7
csodcaceres <cs.odcaceres@gmail.com>
2025-04-08 16:41:13 -0300

    Lista 1

---
+++ lista_1.py
@@ -0,0 +1,16 @@
+# Escribe un código que lee la lista siguiente y realiza:
+
+lista = [16, 14, 63, 65, 17, 99, 70, 11, 20, 48, 79, 32, 17, 89, 12, 25, 66]
+
+# Leer el tamaño de la lista
+tamanio = len(lista)
+
+# Leer el valor maximo y minimo
+mayor = max(lista)
+menor = min(lista)
+
+# Calcular la suma de los valores de la lista
+suma_lista = sum(lista)
+
+# Mostrar un mensaje al final
+print(f'La lista tiene {tamanio} numeros, donde el mayor es {mayor} y el menor es {menor}. La suma de los valores es {suma_lista}')

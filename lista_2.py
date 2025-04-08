c88af3597336987f99056f9027c331688f6e7d65
csodcaceres <cs.odcaceres@gmail.com>
2025-04-08 16:46:23 -0300

    Lista 2

---
+++ lista_2.py
@@ -0,0 +1,12 @@
+# Escribe una función que genere la tabla de multiplicar de un número entero del 1 al 10, 
+# según la elección del usuario. Como ejemplo, para el número 7, 
+# la tabla de multiplicar se debe mostrar en el siguiente formato:
+
+
+def tablaMultiplicar(numero):
+    print(f'Tabla de multiplicar del {numero}')
+
+    for i in range(11):
+        print(f'{numero} x {i} = {numero * i}')
+
+tablaMultiplicar(7)

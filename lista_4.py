8865c1154d47fd9962e6071101ade58d2e376bae
csodcaceres <cs.odcaceres@gmail.com>
2025-04-08 16:57:25 -0300

    Lista 4

---
+++ lista_4.py
@@ -0,0 +1,8 @@
+# Crea una lista de los cuadrados de los números de la siguiente 
+# lista [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]. 
+# Recuerda utilizar las funciones lambda y map() para calcular el cuadrado de cada elemento 
+# de la lista.
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+cuadrados = list(map(lambda x: x ** 2, numeros))
+print(cuadrados)
